b9bed5e31ffb7bdddc0750d339c720f9f21ff767
Igor Racyborynsky <defsan@gmail.com>
2011-07-31 13:43:27 -0400

    Fixing authorization problem on sticker.create

--- app/controllers/stickers_controller.rb
+++ app/controllers/stickers_controller.rb
@@ -1,5 +1,6 @@
 class StickersController < ApplicationController
   before_filter :authenticate_user!, :_get_project, :_get_sprint
+  protect_from_forgery :except => [:create]
   layout false
 
   def show
